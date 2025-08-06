3f9c16cbc2ea88dca6e78bef63112643fe3f91b7
Joe Lopes <2897144+lopes@users.noreply.github.com>
2025-08-06 12:59:15 -0300

    encrypt and decrypt function mockups added

--- src/crypto.rs
+++ src/crypto.rs
@@ -5,5 +5,15 @@
 //! routines, and is responsible for handling cryptographic workflows,
 //! such as file processing, key usage, and optional exfiltration.
 
+use std::io::Error;
+
 /// Encrypted files extension.
 pub const EXTENSION: &'static str = "zombie";
+
+pub fn encrypt() -> Result<(), Error> {
+    todo!()
+}
+
+pub fn decrypt() -> Result<(), Error> {
+    todo!()
+}
